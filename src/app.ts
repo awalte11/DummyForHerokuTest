43f8b87f13214bb404bb076f4665966a5a7bb383
Alex <awalte11@students.towson.edu>
2019-04-06 10:58:20 -0400

    added route file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 // lib/app.ts
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes";
 
 class App {
 
     public app: express.Application;
 
+    public routePrv: Routes = new Routes();
+
     constructor() {
         this.app = express();
-        this.config();        
+        this.config();
+        this.routePrv.routes(this.app);           
     }
 
     private config(): void{
