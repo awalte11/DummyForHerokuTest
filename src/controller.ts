c7b3497c7d33325877983520557da4c32a93e198
Alex <awalte11@students.towson.edu>
2019-04-06 17:21:29 -0400

    Patching up error handling

--- src/controller.ts
+++ src/controller.ts
@@ -7,54 +7,74 @@ import { url } from 'inspector';
 const Task = mongoose.model('Task', TaskSchema);
 export class TaskController{
 
-    public addNewTask (req: Request, res: Response) {                
+    /**
+     * Sends request for new task
+    */
+        public addNewTask (req: Request, res: Response) {                
         let newTask = new Task(req.body);
+
+        if(!newTask.description || newTask.description == "" )// doing this here to catch empty descriptions
+        {
+            res.status(400).json({
+                "errorType" : "Description must not be null or empty."
+            })
+        }
     
         newTask.save((err, task) => {
             if(err){
                 
-                res.status(400).json({
-                    "errorResponse" : err.message
-                }).send();
+                res.send(err);//On the off chance of any other errors...
             }
-            var newUrl : string = "dummy";
-            res.append('Location', "localhost:3000/api/tasks/:id?" + task.id);    
-            res.status(201).send();//TODO figure out header     
+            //Slaps the location to get the thing onto the header
+            res.append('Location', "localhost:3000/api/tasks/" + task.id);    
+            res.status(201).send();    
         });
     }
 
+    //Gets tasks from the database
     public getTasks (req: Request, res: Response) {    
-        console.log('called get all');   
-        if (req.params)
-        {
-            console.log(req.params);
-        }       
+              
         Task.find({}, (err, task) => {
             if(err){
                 res.send(err);
             }
+            //no need for status code command, 200 is defailt
             res.json(task);
-        }).select('-__v');
+        }).select('-__v');//this gets rid of that uncalled for variable mongo adds
         
     }
+
+    //gets one task from the db
     public getTaskWithID (req: Request, res: Response) {
-        console.log('called get singular');           
+                  
         Task.findById(mongoose.Types.ObjectId(req.params.id), (err, task) => {
             if(err){
                 res.send(err);
             }
-            if (!task)
+            else if (!task)//the server doesn't return err here so I detect the missing task and handle the 404
             {
                 res.status(404).json({
-                    "errorResponse" : "No task with ID " + req.params.id + "exists"
+                    "errorResponse" : "No task with ID " + req.params.id + " exists"
                 }).send();
             }
             else res.json(task);
-        }).select('-__v');
+        }).select('-__v');//this gets rid of that uncalled for variable mongo adds
     }
 
-    public updateTask (req: Request, res: Response) {    
-        console.log('called patch singular');          
+    //patches a task
+    public updateTask (req: Request, res: Response) {   
+        
+       if(req.body.description == "" || req.body.description == null)// doing this here to catch empty descriptions
+        {
+            res.status(400).json({
+                "errorType" : "Description must not be null or empty."
+            }).send();
+        }
+        else if (req.body.isComplete == null) {
+            res.status(400).json({
+                "errorType" : "isComplete must not be null."
+            }).send();
+        }else
         Task.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), req.body, { new: true }, (err, task) => {
             if(err){
                 res.status(400).send(err);
