0e0c4767ac67f4a36e65eb8d904a3292bb1607e4
Alex <awalte11@students.towson.edu>
2019-04-06 11:13:03 -0400

    route basics

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,38 @@ export class Routes {
             res.status(200).send({
                 message: 'GET request successfulll!!!!'
             })
-        })               
+        })   
+        // General actions 
+        app.route('/api/tasks') 
+        // GET endpoint 
+        .get((req: Request, res: Response) => {
+        // Get all tasks            
+            res.status(200).send({
+                message: 'GET request successfulll!!!!'
+            })
+        })        
+        // POST endpoint
+        .post((req: Request, res: Response) => {   
+        // Create new task         
+            res.status(200).send({
+                message: 'POST request successfulll!!!!'
+            })
+        })
+
+        // task detail
+        app.route('/api/tasks/:Id')
+        // get specific task
+        .get((req: Request, res: Response) => {
+        // Get a single task detail            
+            res.status(200).send({
+                message: 'GET request successfulll!!!!'
+            })
+        })
+        .patch((req: Request, res: Response) => {
+        // Update a contact           
+            res.status(200).send({
+                message: 'PUT request successfulll!!!!'
+            })
+        })         
     }
 }
