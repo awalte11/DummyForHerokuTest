aa0d0099428c74ceb30b988763700c5a8f69b16a
Alex <awalte11@students.towson.edu>
2019-04-06 17:34:08 -0400

    more error handling/comment work

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,7 @@
-//   /lib/controllers/crmController.ts
+
 import * as mongoose from 'mongoose';
 import { TaskSchema } from './models';
 import { Request, Response } from 'express';
-import { url } from 'inspector';
 
 const Task = mongoose.model('Task', TaskSchema);
 export class TaskController{
@@ -11,20 +10,19 @@ export class TaskController{
      * Sends request for new task
     */
         public addNewTask (req: Request, res: Response) {                
-        let newTask = new Task(req.body);
+        
 
-        if(!newTask.description || newTask.description == "" )// doing this here to catch empty descriptions
+        if(!req.body.description || req.body.description == "" )// doing this here to catch empty descriptions
         {
             res.status(400).json({
                 "errorType" : "Description must not be null or empty."
             })
         }
+        let newTask = new Task();
+        newTask.description = req.body.description;
     
         newTask.save((err, task) => {
-            if(err){
-                
-                res.send(err);//On the off chance of any other errors...
-            }
+            
             //Slaps the location to get the thing onto the header
             res.append('Location', "localhost:3000/api/tasks/" + task.id);    
             res.status(201).send();    
@@ -35,10 +33,6 @@ export class TaskController{
     public getTasks (req: Request, res: Response) {    
               
         Task.find({}, (err, task) => {
-            if(err){
-                res.send(err);
-            }
-            //no need for status code command, 200 is defailt
             res.json(task);
         }).select('-__v');//this gets rid of that uncalled for variable mongo adds
         
@@ -79,7 +73,7 @@ export class TaskController{
             if(err){
                 res.status(400).send(err);
             }
-            res.status(204).send(task);
+            res.status(204).send();
         });
     }
 }
